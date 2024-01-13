fd17b61a00430611f91901bb4b1f15a83b2d44c5
birkat <sudiprauniyar@gmail.com>
2024-01-13 23:25:11 +0545

    project folder setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Titanic_Surviver_Prediction',
+    version='0.0.1',
+    author='Sudip Rauniyar',
+    author_email='sudiprauniyar@gmail.com',
+    packages=find_packages()
+)
